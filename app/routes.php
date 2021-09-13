0d0a5a152b4700eded4f0a631e2ad040662a4739
Carlos Jug <carlos@jug.mx>
2021-09-13 13:06:22 -0500

    configuracion inicial

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,5 @@
 <?php
 	//$this->add('pagina/(.+)','pagina/detalle/$1');
-	$paginas=new Paginas();
-	foreach ($paginas->findAll() as $key => $value) {
-		$this->add($value->url,'pagina/detalle/'.$value->url);
-	}
 	#$this->add('admin','admin');
 	#$this->add('(.+)','admin/$1');
 	//$this->add('blog/(.+)','blog/index/$1');
--- config.php
+++ config.php
@@ -4,15 +4,13 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 date_default_timezone_set("America/Mexico_City");
-define('requiresBD', true);
+define('requiresBD', false);
 define('DB_Engine', 'mysqli');
 define('DB_Server', 'localhost');
 define('DB_Port', false);
 
-define('DB_name', 'flavor');
-define('DB_User', 'flavor');
-define('DB_Password', 'VbAwNU29dVw3OAeM!');
-define('Path', 'https://'.$_SERVER['HTTP_HOST'].'/flavor');
-define('Path_api', 'https://'.$_SERVER['HTTP_HOST'].'/flavor/api');
-define('Path_admin', 'https://'.$_SERVER['HTTP_HOST'].'/flavor/admin');
+define('DB_name', '');
+define('DB_User', '');
+define('DB_Password', '');
+define('Path', 'https://'.$_SERVER['HTTP_HOST'].'/mundo-javer-2021');
 ?>
