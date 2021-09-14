0002cff04a2c3893230ded4f2b3285b4180d90c5
Carlos Jug <carlos@jug.mx>
2021-09-13 20:11:28 -0500

    se completo header

--- app/views/elements/header.php
+++ app/views/elements/header.php
@@ -1,51 +1,49 @@
 <header>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="<?=Path ?>">Navbar</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-	
-		<div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
-			<ul class="navbar-nav mr-auto">
-				<li class="nav-item <?php if($controllerName == "index" and $function == "index") echo "active"; ?>">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Fraccionamientos</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Acerca de Javer</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Conferencias</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Registro para premio</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Contacto</a>
-				</li>
-			</ul>
-			<ul class="navbar-nav ml-auto">
-				<li class="navbar-text">
-					<span class="nav-text" >Siguenos</span>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fa fa-fw fa-facebook"></i></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fa fa-fw fa-youtube"></i></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fa fa-fw fa-instagram"></i></a>
-				</li>
-				<li class="navbar-text px-3">
-					|
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Podcast <i class="fa fa-fw fa-spotify"></i></a>
-				</li>
-			</ul>
-		</div>
-	</nav>
+	<div class="container font-sweet-sans-pro font-10">
+		<a id="logo-principal" class="navbar-brand" href="<?=Path ?>"><img src="<?php echo Path;?>/images/header/logo.png"></a>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-0 pb-0 pl-0">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div id="menu-1" class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Fraccionamientos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Acerca de Javer</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Conferencias</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Registro para premio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contacto</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav ml-auto pr-3">
+					<li class="navbar-text">
+						<span class="nav-text text-white">Siguenos</span>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link px-1" href="#"><i class="fa fa-fw fa-facebook"></i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link px-1" href="#"><i class="fa fa-fw fa-youtube"></i></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link px-1" href="#"><i class="fa fa-fw fa-instagram"></i></a>
+					</li>
+					<li class="navbar-text px-3">
+						|
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="position: relative;" href="#" nowrap>Podcast <i class="fa fa-fw fa-spotify"></i></a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
 </header>
