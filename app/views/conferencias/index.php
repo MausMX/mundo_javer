e017d9dc1a8c13159521efaa0dc6f8c3ead10f7f
francisco <francisco.zamudio@maus-haus.mx>
2021-09-27 17:33:27 -0500

    conferencias y detalle responsive

--- app/views/conferencias/index.php
+++ app/views/conferencias/index.php
@@ -1,25 +1,25 @@
 <div class="container font-poppins">
-    <div class="bg-white rounded p-5 mb-5" style="position: relative;">
+    <div class="bg-white rounded p-5 mb-5 info-conferencias" style="position: relative;">
         <a id="btn-back" href="<?=Path?>"><img src="<?=Path?>/images/conferencias/btn_back.png"></a>
-        <div class="row px-4">
-            <div class="col-lg-9">
-                <h1 class="text-danger font-44 mb-4 mt-3"><strong>Conferencias Javer</strong></h1>
-                <p class="font-18">
+        <div class="row px-4 contenido-conferencias">
+            <div class="col-12 col-md-9 col-lg-9 col-xl-9">
+                <h1 class="text-danger font-44 mb-4 mt-3 title-conf"><strong>Conferencias Javer</strong></h1>
+                <p class="font-18 d-none d-lg-block d-xl-block d-md-block">
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
                 </p>
             </div>
-            <div class="col-lg-2 offset-lg-1 text-right">
+            <div class="col-12 col-md-3 col-lg-2 col-xl-2 offset-lg-1 text-right d-none d-lg-block d-xl-block d-md-flex cont-img-conf">
                 <img src="<?=Path?>/images/conferencias/logo_mundo_javer.png">
             </div>
             <div class="w-100 mb-5"></div>
             <div class="col-lg-6 mb-5">
                 <div class="px-3">
                     <div class="row">
-                        <div class="col-lg-5 rounded-left imagen" style="background: url(<?=Path?>/images/conferencias/video1.png);background-size:cover;">
+                        <div class="col-6 col-lg-5 rounded-left imagen img-conf" style="background: url(<?=Path?>/images/conferencias/video1.png);background-size:cover;">
                             <a class="player" href="https://www.youtube.com/embed/XFQtjhFgxN0" data-toggle="modal" data-target="#youtube"><img src="<?=Path?>/images/conferencias/player.png"></a>
                         </div>
-                        <div class="col-lg-7 contenido bg-dark text-white rounded-right">
-                            <div class="px-2 py-4">
+                        <div class="col-6 col-lg-7 contenido bg-dark text-white rounded-right">
+                            <div class="px-2 py-4 desc-conferencia">
                                 <h2 class="titulo font-18"><strong>Los cuidados más esenciales de tu casa.</strong></h2>
                                 <div class="descripcion font-14">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</div>
                             </div>
@@ -30,11 +30,11 @@
             <div class="col-lg-6 mb-5">
                 <div class="px-3">
                     <div class="row">
-                        <div class="col-lg-5 rounded-left imagen" style="background: url(<?=Path?>/images/conferencias/video2.png);background-size:cover;">
+                        <div class="col-6 col-lg-5 rounded-left imagen img-conf" style="background: url(<?=Path?>/images/conferencias/video2.png);background-size:cover;">
                             <a class="player" href="https://www.youtube.com/embed/XFQtjhFgxN0" data-toggle="modal" data-target="#youtube"><img src="<?=Path?>/images/conferencias/player.png"></a>
                         </div>
-                        <div class="col-lg-7 contenido bg-dark text-white rounded-right">
-                            <div class="px-2 py-4">
+                        <div class="col-6 col-lg-7 contenido bg-dark text-white rounded-right">
+                            <div class="px-2 py-4 desc-conferencia">
                                 <h2 class="titulo font-18"><strong>Los cuidados más esenciales de tu casa.</strong></h2>
                                 <div class="descripcion font-14">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</div>
                             </div>
@@ -45,11 +45,11 @@
             <div class="col-lg-6 mb-5">
                 <div class="px-3">
                     <div class="row">
-                        <div class="col-lg-5 rounded-left imagen" style="background: url(<?=Path?>/images/conferencias/video3.png);background-size:cover;">
+                        <div class="col-6 col-lg-5 rounded-left imagen img-conf" style="background: url(<?=Path?>/images/conferencias/video3.png);background-size:cover;">
                             <a class="player" href="https://www.youtube.com/embed/XFQtjhFgxN0" data-toggle="modal" data-target="#youtube"><img src="<?=Path?>/images/conferencias/player.png"></a>
                         </div>
-                        <div class="col-lg-7 contenido bg-dark text-white rounded-right">
-                            <div class="px-2 py-4">
+                        <div class="col-6 col-lg-7 contenido bg-dark text-white rounded-right">
+                            <div class="px-2 py-4 desc-conferencia">
                                 <h2 class="titulo font-18"><strong>Los cuidados más esenciales de tu casa.</strong></h2>
                                 <div class="descripcion font-14">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</div>
                             </div>
@@ -60,11 +60,11 @@
             <div class="col-lg-6 mb-5">
                 <div class="px-3">
                     <div class="row">
-                        <div class="col-lg-5 rounded-left imagen" style="background: url(<?=Path?>/images/conferencias/video4.png);background-size:cover;">
+                        <div class="col-6 col-lg-5 rounded-left imagen img-conf" style="background: url(<?=Path?>/images/conferencias/video4.png);background-size:cover;">
                             <a class="player" href="https://www.youtube.com/embed/XFQtjhFgxN0" data-toggle="modal" data-target="#youtube"><img src="<?=Path?>/images/conferencias/player.png"></a>
                         </div>
-                        <div class="col-lg-7 contenido bg-dark text-white rounded-right">
-                            <div class="px-2 py-4">
+                        <div class="col-6 col-lg-7 contenido bg-dark text-white rounded-right">
+                            <div class="px-2 py-4 desc-conferencia">
                                 <h2 class="titulo font-18"><strong>Los cuidados más esenciales de tu casa.</strong></h2>
                                 <div class="descripcion font-14">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</div>
                             </div>
--- app/views/desarrollos/index.php
+++ app/views/desarrollos/index.php
@@ -2,12 +2,12 @@
     <div class="row" style="position: relative;">
 		<div class="col-6 d-block">
         	<a class="btn btn-danger pl-2 pr-3 text-uppercase font-10 poppins py-2 prev-frac" href="<?= $prevfrac;?>">
-        		<i class="fas fa-caret-left pr-3"></i><strong>Anterior Fraccionamiento</strong>
+        		<i class="fas fa-caret-left pr-3 icon-previ"></i><strong>Anterior Fraccionamiento</strong>
         	</a>
 		</div>
 		<div class="col-6 d-flex" style="justify-content:right;">
 			<a class="btn btn-danger pl-3 pr-2 text-uppercase font-10 poppins py-2 next-frac" href="<?= $nextfrac;?>">
-        		<strong>Siguiente Fraccionamiento</strong><i class="fas fa-caret-right pl-3"></i>
+        		<strong>Siguiente Fraccionamiento</strong><i class="fas fa-caret-right pl-3 icon-netx"></i>
         	</a>
 		</div>
 	</div>
@@ -39,34 +39,62 @@
 				  </a>
 				</div>
             </div>
-           <div class="col-lg-12 bg-white" style="position: relative;">
-                <div class="p-5">
-                    <div class="px-5 py-3">
+           <div class="col-12 col-lg-12 bg-white" style="position: relative;">
+                <div class="p-5 padding-line">
+                    <div class="px-5 py-3 padding-extra">
                             <div class="row align-items-center">
-                                <div class="col-lg-4">
+                                <div class="col-8 offset-2 offset-md-0 offset-lg-0 offset-xl-0 col-lg-4 col-xl-4 col-md-4 d-flex div-logo-des">
                 					<img src="<? echo Path.$logo?>"> 
                                 </div>
-                                <div class="col-lg-7">
-                                    <p class="font-23 poppins"><?=$descripcion;?></p>                                   
+                                <div class="col-12  col-md-8 col-lg-8">
+                                    <p class="font-23 poppins desc-desarrollo"><?=$descripcion;?></p>                                   
                                 </div>  
-                                <div class="col-lg-4">
-                                </div>
-                                <div class="col-lg-8 d-flex">
-                                    <div class="col-lg-1 pr-0">
-                                    	<img src="<?=Path?>/images/desarrollo/alvento/location.png">
-                                    </div>
-                                    <div class="col-lg-4 pl-0 pr-0">
-	                                	<p class="mb-0 font-17 bold DA291C">Ubicación:</p>
-	                                	<p class="mt-0 font-17 location-name"><?=$ubicacion;?></p>
-                                   	</div> 
-                                   	<div class="col-lg-2 pr-0 pl-0 img-ingresos">
-                                    	<img src="<?=Path?>/images/desarrollo/alvento/ingresos.png">
-                                    </div>
-                                    <div class="col-lg-4 pl-2">
-	                                	<p class="mb-0 font-17 bold DA291C">Ingresos:</p>
-	                                	<p class="mt-0 font-17 location-name"><?=$leyenda2;?></p>
-                                   	</div>                                     
-                                </div>                                 
+                                <!--<div class=" col-12 col-lg-4">
+                                </div>-->
+                                <!--<div class="col-12 col-lg-8 offset-lg-4 d-lg-flex d-xl-flex d-block">
+                                	<div class="col-10 offset-2 col-lg-6 p-0 d-flex info-ubicacion">                                    
+	                                    <div class="col-2 col-lg-2 pr-0 img-ubicacion">
+	                                    	<img src="<?=Path?>/images/desarrollo/alvento/location.png">
+	                                    </div>
+	                                    <div class="col-10 col-lg-10 col-xl-10 col-lg-10 pl-2 pr-0 contenido-ubicacion">
+		                                	<p class="mb-0 font-17 bold DA291C">Ubicación:</p>
+		                                	<p class="mt-0 font-17 location-name"><?=$ubicacion;?></p>
+	                                   	</div> 
+	                                </div>
+                                	<div class="col-10 offset-2 col-lg-6 p-0 d-flex info-ingresos">                                    
+	                                   	<div class="col-2 col-lg-2 pr-0 pl-0 img-ingresos">
+	                                    	<img src="<?=Path?>/images/desarrollo/alvento/ingresos.png">
+	                                    </div>
+	                                    <div class="col-10 col-lg-10 col-xl-10 pl-2">
+		                                	<p class="mb-0 font-17 bold DA291C">Ingresos:</p>
+		                                	<p class="mt-0 font-17 location-name"><?=$leyenda2;?></p>
+	                                   	</div>
+	                                </div>                                     
+                                </div>--> 
+                                <div class="col-12 col-lg-8 offset-lg-4 col-xl-8 offset-xl-4 col-md-8 offset-md-4 d-md-flex d-lg-flex d-xl-flex d-block">
+                                	<div class="col-12 col-lg-6 col-xl-6 col-md-6 p-0 d-flex info-ubicacion">                                    
+	                                    <div class="col-12 col-lg-12 pr-0 img-ubicacion">
+	                                    	<div class="f-left">
+	                                    		<img src="<?=Path?>/images/desarrollo/alvento/location.png">
+	                                    	</div>	
+	                                    	<div class="f-left pl-2">                                    
+			                                	<p class="mb-0 font-17 bold DA291C">Ubicación:</p>
+			                                	<p class="mt-0 font-17 location-name"><?=$ubicacion;?></p>
+			                                </div>
+	                                   	</div> 
+	                                </div>
+                                	<div class="col-12 col-lg-6 col-xl-6 col-md-6 p-0 d-flex info-ingresos">                                    
+	                                   	<div class="col-12 col-lg-12 pr-0 pl-0 img-ingresos">
+	                                    	<div class="f-left">
+	                                    		<img src="<?=Path?>/images/desarrollo/alvento/ingresos.png">
+	                                    	</div>	
+	                                    	<div class="f-left pl-2">                                    
+			                                	<p class="mb-0 font-17 bold DA291C">Ingresos:</p>
+			                                	<p class="mt-0 font-17 location-name"><?=$leyenda2;?></p>
+			                                </div>
+	                                   	</div>
+	                                </div>                                     
+                                </div>                                
                             </div>
                     </div>
                 </div>
@@ -80,29 +108,29 @@
            	<?
             }
             ?>
-           <div class="col-lg-12 bg-white py-5" style="position: relative;">
+           <div class="col-lg-12 bg-white py-5 rounded-bottom" style="position: relative;">
            		<div class="col-lg-12 interes-desarrollo mb-5">
 	       			<p class="mb-0 font-30 poppins bold DA291C">¿Te interesa este fraccionamiento?</p>
 	       		</div>
-	       		<div class="col-lg-4 offset-2 f-left">
-	       			<div class="col-lg-12 pr-0 pl-0 mb-3">
+	       		<div class="col-lg-4 offset-lg-2 col-md-5 col-xl-4 offset-xl-2 offset-md-1 f-left">
+	       			<div class="col-lg-12 pr-0 pl-0 mb-3 oficina-title">
 		            	<p class="mb-0 font-18 poppins bold">Visita la Oficina de Venta</p>
 	                </div>
-		       		<div class="col-lg-1 pr-0 pl-0 f-left text-center">
+		       		<div class="col-lg-1 col-xl-1 col-md-1 pr-0 pl-0 f-left text-center d-none d-lg-block d-xl-block d-md-block">
 	                	<img src="<?=Path?>/images/desarrollo/alvento/location_red.png">
 	                </div>
-	                <div class="col-lg-11 pl-2 f-left mb-4">
+	                <div class="col-lg-11 col-xl-11 col-md-11 pl-2 f-left mb-4 d-none d-lg-block d-xl-block d-md-block">
 		            	<p class="mb-0 font-16 poppins"><?= $calle;?></p>
 	                </div>
-	                <div class="col-lg-11 pl-2">
+	                <div class="col-lg-11  col-xl-11 col-md-11 pl-2 btn-location">
                     <a class="btn btn-danger pl-3 pr-4 text-uppercase font-10 poppins py-2" target="_blank" href="<?= $maps?>"><img class="pr-1 logo-location-maps" src="<?=Path?>/images/desarrollo/location_google.png"><strong>Abrir en google maps</strong></a>
 	                </div>
 	            </div>
-	            <div class="col-lg-4 f-left offset-1">
-	       			<div class="col-lg-12 pr-0 pl-0 mb-4">
+	            <div class="col-lg-4 f-left offset-lg-0 col-md-6 col-xl-4">
+	       			<div class="col-lg-12 pr-0 pl-0 mb-4 title-asesores">
 		            	<p class="mb-0 font-18 poppins bold">Contacta a uno de nuestros asesores</p>
 	                </div>
-		       		<div class="col-lg-11 pl-2">
+		       		<div class="col-lg-11 pl-2 btn-contacta">
                     	<a class="btn btn-danger pl-3 pr-4 text-uppercase font-10 poppins py-2" href="<?=Path?>"><img class="pr-1 logo-wp-btn" src="<?=Path?>/images/desarrollo/wp-btn.png"><strong>Contactar asesor</strong></a>
 	                </div>
 	            </div>
