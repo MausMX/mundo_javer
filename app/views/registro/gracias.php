e556119080c25be12862af8aa77679cfa0f7f348
francisco <francisco.zamudio@maus-haus.mx>
2021-09-28 17:52:52 -0500

    gracias registro responsive

--- app/views/registro/gracias.php
+++ app/views/registro/gracias.php
@@ -1,25 +1,28 @@
 <div class="container font-poppins">
-    <div class="row justify-content-center">
-        <div class="col-lg-10 col-md-12 px-3 mb-5">
-            <div class="row bg-white rounded py-5" style="position: relative;">
-                <a id="btn-back" href="<?=Path?>"><img src="<?=Path?>/images/conferencias/btn_back.png"></a>
-                <div class="col-xl-3 col-lg-4 p-0">
-                    <img src="<?=Path?>/images/registro/img_gracias.png">
-                </div>
-                <div class="col-xl-9 col-lg-8" style="position: relative;">
-                    <div class="px-5 py-4">
-                        <div class="px-3">
-                            <h1 class="text-danger font-44 line-height-32 font-poppins mb-5"><strong>¡Gracias por tu registro!</strong></h1>
-                            <p class="font-poppins font-18 mb-5">
-                                <strong>¡Gracias por tu registro y ser parte de Mundo Javer!</strong><br><br>
-                                <strong>En breve serás contactado por uno de nuestros asesores. </strong></p>
-                            <a class="btn btn-danger py-2 px-4 font-10 mb-3 mt-2" href="<?=Path?>"><strong>Continuar al mundo javer</strong></a>
-                            <img style="position: absolute;bottom: 0px;right: 50px;width: 10%;" src="<?=Path?>/images/conferencias/logo_mundo_javer.png">
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-    </div>
+    <div class="px-3">
+	    <div class="row justify-content-center">
+	        <div class="col-lg-10 col-md-10 px-3 mb-5">
+	            <div class="row bg-white rounded py-5" style="position: relative;">
+	                <a id="btn-back" href="<?=Path?>"><img src="<?=Path?>/images/conferencias/btn_back.png"></a>
+	                <div class="col-8 offset-2 offset-md-0 offset-lg-0 offset-xl-0 col-xl-3 col-lg-4 col-md-3 p-0">
+	                    <img class="d-none d-lg-block d-xl-block d-md-block" src="<?=Path?>/images/registro/img_gracias.png">
+	                    <img class="d-block d-lg-none d-xl-none d-md-none mobile-logo-gracias" src="<?=Path?>/images/registro/logo_gracias_mobile.png">
+	                </div>
+	                <div class="col-md-9 col-xl-9 col-lg-8" style="position: relative;">
+	                    <div class="px-5 py-4 contenido-gracias">
+	                        <div class="px-3">
+	                            <h1 class="text-danger font-44 line-height-32 font-poppins mb-5"><strong>¡Gracias por tu registro!</strong></h1>
+	                            <p class="font-poppins font-18 mb-5">
+	                                <strong>¡Gracias por tu registro y ser parte de Mundo Javer!</strong><br><br>
+	                                <strong>En breve serás contactado por uno de nuestros asesores. </strong></p>
+	                            <a class="btn btn-danger py-2 px-4 font-10 btn-continuar mb-3 mt-2" href="<?=Path?>"><strong>Continuar al mundo javer</strong></a>
+	                            <img class="d-none d-lg-block d-xl-block" style="position: absolute;bottom: 0px;right: 50px;width: 10%;" src="<?=Path?>/images/conferencias/logo_mundo_javer.png">
+	                        </div>
+	                    </div>
+	                </div>
+	                
+	            </div>
+	        </div>
+	    </div>
+	</div>
 </div>
