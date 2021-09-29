b29b60836bc8e6a3a92f0392390e277645b44739
Carlos Jug <carlos@jug.mx>
2021-09-28 20:42:28 -0500

    se termino acerca de

--- app/views/acerca/index.php
+++ app/views/acerca/index.php
@@ -2,16 +2,53 @@
     <div class="mb-5">
         <div class="bg-white rounded" style="position: relative;">
             <a id="btn-back" href="<?=Path?>" style="z-index:10;"><img src="<?=Path?>/images/conferencias/btn_back.png"></a>
-            <div class="row pt-5 pr-3 pb-5 pl-5">
-                <div class="col-lg-6">
+            <div class="row pt-5 pb-4 px-5">
+                <div class="col-lg-7">
+                    <div class="px-4">
+                        <h1 class="font-poppins font-44 line-height-44 text-danger mb-5"><strong>Mundo Javer<br> Es el momento de<br> tu oportunidad</strong></h1>
+                        <p class="text-justify mb-4"><strong>La situación actual nos ha permitido adaptarnos a un sinfín de nuevas tecnologías, auxiliares de nuestra nueva realidad.</strong></p>
+                        <p class="text-justify">En Mundo Javer apostamos por las nuevas experiencias virtuales. Adaptándonos a tus necesidades, te presentamos la nueva edición de Mundo Javer: una expo inmobiliaria completamente virtual.</p>
+                    </div>
+                </div>
+                <div class="offset-lg-1 col-lg-4 offset-xl-2 col-xl-3">
+                    <div class="pr-4">
+                        <img src="<?=Path?>/images/acerca/logo_mundo_javer.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="pt-5 pb-4 px-5">
+                <div class="px-4">
+                    <h1 class="font-poppins font-44 text-danger mb-5"><strong>El mundo de las oportunidades</strong></h1>
+                    <p class="text-justify mb-4"><strong>Mundo Javer es una experiencia única e interactiva, sin dejar de lado la atención personalizada que requieras, donde te acompañamos en todos los procesos para realizar una compra inmobiliaria con ventajas adicionales.</strong></p>
+                    <p class="text-justify">Cada una de las compras realizadas, ofrecen recompensas especiales que complementan tu experiencia al adquirir una nueva vivienda, como premios comprometidos con el cuidado de nuestro medio ambiente y más sorpresas, lo que hace a Mundo Javer el mundo de las oportunidades.</p>
+                </div>
+            </div>
+            <div class="row py-5 px-5">
+                <div class="col-md-6">
+                    <div class="px-4">
+                        <h1 class="font-poppins font-30 text-danger mb-5"><strong>Experiencias interactivas</strong></h1>
+                        <p class="text-justify">La expo Mundo Javer es como cualquier otra expo inmobiliaria, pero disponible en cualquier parte donde estés. Su mapa interactivo te permite visualizar todos nuestros desarrollos disponibles y los estados donde se encuentran. Como una expo física, también podrás disfrutar de eventos especiales y conferencias para facilitarte el proceso de elección de acuerdo a lo que estás buscando.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="px-4">
+                        <h1 class="font-poppins font-30 text-danger mb-5"><strong>Opciones para ti</strong></h1>
+                        <p class="text-justify">Ofrecemos desarrollos de diversas características, donde encontrarás el inmueble perfecto para ti, de acuerdo a lo que estés buscando: espacios designados para el home office, zonas de áreas verdes y de actividades, etc.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row px-5 pb-5 align-items-center">
+                <div class="col-lg-5">
                     <div class="pl-4">
-                        <h1 class="text-danger font-37 line-height-44 font-poppins mb-5"><strong>Acerca de Javer</strong></h1>
+                        <img src="<?=Path?>/images/acerca/acerca_de_javer.jpg">
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="pr-4">
+                        <h1 class="font-37 line-height-44 font-poppins mb-4"><strong>Acerca de Javer</strong></h1>
                         <p class="mb-4 font-poppins font-18">Somos la empresa desarrolladora de vivienda con más prestigio en el norte de México con sede en Monterrey, Nuevo León. Iniciamos operaciones en 1973 a cargo del Ing. Salomón Marcuschamer y hoy, contamos con más de 47 años de experiencia en desarrollo inmobiliario.</p>
                         <p class="font-poppins font-18">Nos especializamos en desarrollos de alta calidad para diferentes segmentos del mercado.</p>
                     </div>
-                </div>
-                <div class="col-lg-6 pl-5">
-                    <img style="position: relative;margin-top:-19%;" src="<?=Path?>/images/acerca/img_1.png">
                 </div>
             </div>
             <div class="bg-danger text-white p-5" style="background: url(<?=Path?>/images/acerca/img2.png) center center no-repeat;background-size:contain;">
@@ -40,17 +77,28 @@
             </div>
             <section id="responsabilidad_social" class="pt-5 px-5">
                 <div class="p-3">
+                    <h1 class="font-poppins font-44 mb-4"><strong>Responsabilidad Social</strong></h1>
                     <div class="row">
                         <div class="col-lg-8">
-                            <h1 class="font-poppins font-44 mb-4"><strong>Responsabilidad Social</strong></h1>
-                            <p class="font-poppins font-18">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra dui a aliquet iaculis. Integer sed blandit odio, gravida laoreet mi. Nunc in porttitor purus. Aliquam quam arcu, iaculis a ex vel, elementum dapibus risus. Phasellus porttitor sem mi, rhoncus molestie lectus pulvinar pellentesque.</p>
+                            <p class="font-poppins font-18 pr-3 text-justify">Somos una Empresa Socialmente Responsable desde 2017, labor reflejada a través de nuestro fiel compromiso con el cuidado del medio ambiente; es por eso que nuestras opciones de regalos al adquirir tu hogar en Mundo Javer, son completamente ecológicas. Fuimos reconocidos por Great Place To Work con el 4to lugar en su ranking 2020 Región Noreste.</p>
                         </div>
                         <div class="col-lg-4 text-right">
-                            <img src="<?=Path?>/images/acerca/img_responsabilidad.png">
+                            <table width="100%">
+                                <tr>
+                                    <td width="40%">
+                                        <img src="<?=Path?>/images/acerca/img_responsabilidad.png">
+                                    </td>
+                                    <td>
+                                        <img src="<?=Path?>/images/acerca/logo_empresa_socialmente_responsable.jpg">
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
+                    <a class="btn btn-danger px-4 text-uppercase font-10 font-sweet-sans-pro py-2 mt-4 mb-5" href="<?=Path?>"><strong>Regresar al mundo Javer</strong></a>
                 </div>
             </section>
+            <?php /*
             <section id="experiencia_javer" class="px-5 pb-5">
                 <div class="p-3">
                     <div class="row mb-3">
@@ -76,9 +124,9 @@
                             </div>
                         </div>
                     </div>
-                    <a class="btn btn-danger px-4 text-uppercase font-10 font-sweet-sans-pro py-2" href="<?=Path?>"><strong>Regresar al mundo Javer</strong></a>
                 </div>
             </section>
+            */ ?>
         </div>
     </div>
 </div>
