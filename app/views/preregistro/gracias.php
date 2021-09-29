8f86fa33ca564fb5b3753f1431b4e867677f039c
francisco <francisco.zamudio@maus-haus.mx>
2021-09-29 16:57:24 -0500

    espaciado

--- app/views/preregistro/gracias.php
+++ app/views/preregistro/gracias.php
@@ -1,7 +1,7 @@
 <div class="container font-poppins">
     <div class="px-3">
         <div class="row justify-content-center">
-            <div class="col-lg-10 col-md-10 px-3 mb-5">
+            <div class="col-lg-10 col-md-10 px-3 mb-0">
                 <div class="row bg-white rounded py-3 align-items-center" style="position: relative;">
                     <a id="btn-back" href="<?=Path?>"><img src="<?=Path?>/images/conferencias/btn_back.png"></a>
                     <div class="col-8 offset-2 offset-md-0 offset-lg-0 offset-xl-0 col-xl-4 col-lg-4 col-md-3 p-0">
@@ -13,7 +13,7 @@
                             <div class="">
                                 <h1 class="text-success font-44 line-height-32 font-poppins mb-5"><strong>¡Gracias por tu registro!</strong></h1>
                                 <p class="font-poppins font-18 mb-3">
-                                    <strong>Estás a un paso más cerca de encontrar tu nuevo hogar con increíbles regalos sustentables y ecológicos.</strong>
+                                    <strong>Estás un paso más cerca de encontrar tu nuevo hogar con increíbles regalos sustentables y ecológicos.</strong>
                                 </p>
                                 <p>Te notificaremos el día del inicio de Mundo Javer.</p>
                                 <img class="d-none d-lg-block d-xl-block" style="position: absolute;bottom: -25px;right: 50px;width: 10%;" src="<?=Path?>/images/conferencias/logo_mundo_javer.png">
