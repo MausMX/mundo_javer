7c914af981e609cc5f26d45e345b01c1a3364b45
Carlos Jug <carlos@jug.mx>
2021-10-06 13:53:36 -0500

    se agrego link spotify

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,3 +1,4 @@
+<style>body{background: transparent;}</style>
 <div id="area_mapa">
     <div id="mapa">
         <div class="animation" id="entorno"></div>
@@ -5,7 +6,7 @@
         <div class="animation" id="aguascalientes"></div>
         <div class="animation" id="corporativo"></div>
         <div class="animation" id="edomex"></div>
-        <div class="animation" id="globo"></div>
+        <div class="animation" id="globo"><a target="_blank" href="https://open.spotify.com/show/2y8p0X9aATy4AS0MoOOFHJ?si=OXalRj0DSjynkRTtxBoZJA&dl_branch=1">&nbsp;</a></div>
         <div class="animation" id="jalisco"></div>
         <div class="animation" id="queretaro"></div>
         <div class="animation" id="quintanaroo"></div>
