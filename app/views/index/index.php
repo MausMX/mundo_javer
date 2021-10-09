2e8325a9a0573131e8c98f1c529cebfb1bcd410d
Carlos Jug <carlos@jug.mx>
2021-10-08 21:24:27 -0500

    se agrego pin en los estados

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -7,16 +7,16 @@
         <div id="edificio_4"></div>
         <div id="edificio_7"></div>
         <div id="edificio_8"></div>
-        <div class="animation" id="nuevoleon"></div>
-        <div class="animation" id="aguascalientes"></div>
-        <div class="animation" id="corporativo"></div>
-        <div class="animation" id="edomex"></div>
+        <div class="animation" id="nuevoleon"><div class="pin animation"></div></div>
+        <div class="animation" id="aguascalientes"><div class="pin animation"></div></div>
+        <div class="animation" id="corporativo"><div class="pin animation"></div></div>
+        <div class="animation" id="edomex"><div class="pin animation"></div></div>
         <div class="animation" id="globo"><a target="_blank" href="https://open.spotify.com/show/2y8p0X9aATy4AS0MoOOFHJ?si=OXalRj0DSjynkRTtxBoZJA&dl_branch=1">&nbsp;</a></div>
-        <div class="animation" id="jalisco"></div>
-        <div class="animation" id="queretaro"></div>
-        <div class="animation" id="quintanaroo"></div>
+        <div class="animation" id="jalisco"><div class="pin animation"></div></div>
+        <div class="animation" id="queretaro"><div class="pin animation"></div></div>
+        <div class="animation" id="quintanaroo"><div class="pin animation"></div></div>
         <div class="animation" id="regalo"></div>
-        <div class="animation" id="tamaulipas"></div>
+        <div class="animation" id="tamaulipas"><div class="pin animation"></div></div>
         <div class="animation" id="edif_agu_jal"></div>
         <div id="camion"><div id="camion1"></div><div id="camion2"></div><div id="camion3"></div><div id="camion4"></div></div>
         <div id="carro_1"><div id="carro_1_1"></div><div id="carro_1_2"></div><div id="carro_1_3"></div><div id="carro_1_4"></div></div>
