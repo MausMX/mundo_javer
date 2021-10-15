0124bee0f9d52ba07c7c1fa0bb7654c9858d5721
francisco <francisco.zamudio@maus-haus.mx>
2021-10-14 22:30:27 -0500

    footer links

--- app/views/elements/footer.php
+++ app/views/elements/footer.php
@@ -23,10 +23,10 @@
 					<span class="c-white"><img class="mr-2" src="<?=Path?>/images/footer/logo_footer.png"></span>
 				</div>
 				<div class="py-3 mx-3 font-10 info-footer">
-					<a id="footer-btn-aviso" class="text-secondary" href="https://www.javer.com.mx/avisos-de-privacidad#clientes" target="_blank"><span class="c-white font-sweet-sans- text-uppercase">Aviso de privacidad</span></a>
+					<a id="footer-btn-aviso" class="text-secondary" href="https://www.javer.com.mx/avisos-de-privacidad#clientes" target="_blank"><span id="footer-btn-aviso-texto" class="c-white font-sweet-sans- text-uppercase">Aviso de privacidad</span></a>
 				</div> 
 				<div class="py-3 mx-3 font-10 info-footer">
-					<a id="footer-btn-terminos" class="text-secondary" href="<?=Path?>/terminos"><span class="c-white font-sweet-sans- text-uppercase">Términos y Condiciones</span></a>
+					<a id="footer-btn-terminos" class="text-secondary" href="<?=Path?>/terminos"><span id="footer-btn-terminos-texto" class="c-white font-sweet-sans- text-uppercase">Términos y Condiciones</span></a>
 				</div>
 				<div class="mx-3 py-2 font-10 info-footer"> <span class="c-white font-sweet-sans- text-uppercase">Copyright © casas javer 2021. Todos los derechos reservados</span></div>
 				<?php
@@ -36,7 +36,7 @@
 				}else{ 
 				?>
 				<div class="mx-3 py-2 font-10 info-footer regalo-footer d-none d-lg-block d-xl-block">
-						<a id="footer-btn-regalo" target="_blank" class="d-inline-block" href="<?= Path?>/registro/pasos_regalo"><img src="<?=Path?>/images/preregistro/regalo_preheat.png"></a>
+						<a id="footer-btn-regalo" target="_blank" class="d-inline-block" href="<?= Path?>/registro/pasos_regalo"><img id="footer-btn-regalo-img" src="<?=Path?>/images/preregistro/regalo_preheat.png"></a>
 				</div>
 				<?}?>
 			</div>
