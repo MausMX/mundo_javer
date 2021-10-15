3018c3c4ca1f7585284ba533ab4420598d382022
Carlos Jug <carlos@jug.mx>
2021-10-15 11:38:04 -0500

    se agregaron id a los pasos del regalo

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -130,97 +130,97 @@
         </div>
         <div id="regalo">
             <a id="mapa-btn-pasos-regalo" class="d-block w-100" style="height: 100%;" href="<?=Path?>/registro/pasos_regalo">
-            <div class="regalo_1"></div>
-            <div class="regalo_2"></div>
-            <div class="regalo_3"></div>
-            <div class="regalo_4"></div>
-            <div class="regalo_5"></div>
-            <div class="regalo_6"></div>
-            <div class="regalo_7"></div>
-            <div class="regalo_8"></div>
-            <div class="regalo_9"></div>
-            <div class="regalo_10"></div>
-            <div class="regalo_11"></div>
-            <div class="regalo_12"></div>
-            <div class="regalo_13"></div>
-            <div class="regalo_14"></div>
-            <div class="regalo_15"></div>
-            <div class="regalo_16"></div>
-            <div class="regalo_17"></div>
-            <div class="regalo_18"></div>
-            <div class="regalo_19"></div>
-            <div class="regalo_20"></div>
-            <div class="regalo_21"></div>
-            <div class="regalo_22"></div>
-            <div class="regalo_23"></div>
-            <div class="regalo_24"></div>
-            <div class="regalo_25"></div>
-            <div class="regalo_26"></div>
-            <div class="regalo_27"></div>
-            <div class="regalo_28"></div>
-            <div class="regalo_29"></div>
-            <div class="regalo_30"></div>
-            <div class="regalo_31"></div>
-            <div class="regalo_32"></div>
-            <div class="regalo_33"></div>
-            <div class="regalo_34"></div>
-            <div class="regalo_35"></div>
-            <div class="regalo_36"></div>
-            <div class="regalo_37"></div>
-            <div class="regalo_38"></div>
-            <div class="regalo_39"></div>
-            <div class="regalo_40"></div>
-            <div class="regalo_41"></div>
-            <div class="regalo_42"></div>
-            <div class="regalo_43"></div>
-            <div class="regalo_44"></div>
-            <div class="regalo_45"></div>
-            <div class="regalo_46"></div>
-            <div class="regalo_47"></div>
-            <div class="regalo_48"></div>
-            <div class="regalo_49"></div>
-            <div class="regalo_50"></div>
-            <div class="regalo_51"></div>
-            <div class="regalo_52"></div>
-            <div class="regalo_53"></div>
-            <div class="regalo_54"></div>
-            <div class="regalo_55"></div>
-            <div class="regalo_56"></div>
-            <div class="regalo_57"></div>
-            <div class="regalo_58"></div>
-            <div class="regalo_59"></div>
-            <div class="regalo_60"></div>
-            <div class="regalo_61"></div>
-            <div class="regalo_62"></div>
-            <div class="regalo_63"></div>
-            <div class="regalo_64"></div>
-            <div class="regalo_65"></div>
-            <div class="regalo_66"></div>
-            <div class="regalo_67"></div>
-            <div class="regalo_68"></div>
-            <div class="regalo_69"></div>
-            <div class="regalo_70"></div>
-            <div class="regalo_71"></div>
-            <div class="regalo_72"></div>
-            <div class="regalo_73"></div>
-            <div class="regalo_74"></div>
-            <div class="regalo_75"></div>
-            <div class="regalo_76"></div>
-            <div class="regalo_77"></div>
-            <div class="regalo_78"></div>
-            <div class="regalo_79"></div>
-            <div class="regalo_80"></div>
-            <div class="regalo_81"></div>
-            <div class="regalo_82"></div>
-            <div class="regalo_83"></div>
-            <div class="regalo_84"></div>
-            <div class="regalo_85"></div>
-            <div class="regalo_86"></div>
-            <div class="regalo_87"></div>
-            <div class="regalo_88"></div>
-            <div class="regalo_89"></div>
-            <div class="regalo_90"></div>
-            <div class="regalo_91"></div>
+            <div id="mapa-btn-pasos-regalo-img1" class="regalo_1"></div>
+            <div id="mapa-btn-pasos-regalo-img2" class="regalo_2"></div>
+            <div id="mapa-btn-pasos-regalo-img3" class="regalo_3"></div>
+            <div id="mapa-btn-pasos-regalo-img4" class="regalo_4"></div>
+            <div id="mapa-btn-pasos-regalo-img5" class="regalo_5"></div>
+            <div id="mapa-btn-pasos-regalo-img6" class="regalo_6"></div>
+            <div id="mapa-btn-pasos-regalo-img7" class="regalo_7"></div>
+            <div id="mapa-btn-pasos-regalo-img8" class="regalo_8"></div>
+            <div id="mapa-btn-pasos-regalo-img9" class="regalo_9"></div>
+            <div id="mapa-btn-pasos-regalo-img10" class="regalo_10"></div>
+            <div id="mapa-btn-pasos-regalo-img11" class="regalo_11"></div>
+            <div id="mapa-btn-pasos-regalo-img12" class="regalo_12"></div>
+            <div id="mapa-btn-pasos-regalo-img13" class="regalo_13"></div>
+            <div id="mapa-btn-pasos-regalo-img14" class="regalo_14"></div>
+            <div id="mapa-btn-pasos-regalo-img15" class="regalo_15"></div>
+            <div id="mapa-btn-pasos-regalo-img16" class="regalo_16"></div>
+            <div id="mapa-btn-pasos-regalo-img17" class="regalo_17"></div>
+            <div id="mapa-btn-pasos-regalo-img18" class="regalo_18"></div>
+            <div id="mapa-btn-pasos-regalo-img19" class="regalo_19"></div>
+            <div id="mapa-btn-pasos-regalo-img20" class="regalo_20"></div>
+            <div id="mapa-btn-pasos-regalo-img21" class="regalo_21"></div>
+            <div id="mapa-btn-pasos-regalo-img22" class="regalo_22"></div>
+            <div id="mapa-btn-pasos-regalo-img23" class="regalo_23"></div>
+            <div id="mapa-btn-pasos-regalo-img24" class="regalo_24"></div>
+            <div id="mapa-btn-pasos-regalo-img25" class="regalo_25"></div>
+            <div id="mapa-btn-pasos-regalo-img26" class="regalo_26"></div>
+            <div id="mapa-btn-pasos-regalo-img27" class="regalo_27"></div>
+            <div id="mapa-btn-pasos-regalo-img28" class="regalo_28"></div>
+            <div id="mapa-btn-pasos-regalo-img29" class="regalo_29"></div>
+            <div id="mapa-btn-pasos-regalo-img30" class="regalo_30"></div>
+            <div id="mapa-btn-pasos-regalo-img31" class="regalo_31"></div>
+            <div id="mapa-btn-pasos-regalo-img32" class="regalo_32"></div>
+            <div id="mapa-btn-pasos-regalo-img33" class="regalo_33"></div>
+            <div id="mapa-btn-pasos-regalo-img34" class="regalo_34"></div>
+            <div id="mapa-btn-pasos-regalo-img35" class="regalo_35"></div>
+            <div id="mapa-btn-pasos-regalo-img36" class="regalo_36"></div>
+            <div id="mapa-btn-pasos-regalo-img37" class="regalo_37"></div>
+            <div id="mapa-btn-pasos-regalo-img38" class="regalo_38"></div>
+            <div id="mapa-btn-pasos-regalo-img39" class="regalo_39"></div>
+            <div id="mapa-btn-pasos-regalo-img40" class="regalo_40"></div>
+            <div id="mapa-btn-pasos-regalo-img41" class="regalo_41"></div>
+            <div id="mapa-btn-pasos-regalo-img42" class="regalo_42"></div>
+            <div id="mapa-btn-pasos-regalo-img43" class="regalo_43"></div>
+            <div id="mapa-btn-pasos-regalo-img44" class="regalo_44"></div>
+            <div id="mapa-btn-pasos-regalo-img45" class="regalo_45"></div>
+            <div id="mapa-btn-pasos-regalo-img46" class="regalo_46"></div>
+            <div id="mapa-btn-pasos-regalo-img47" class="regalo_47"></div>
+            <div id="mapa-btn-pasos-regalo-img48" class="regalo_48"></div>
+            <div id="mapa-btn-pasos-regalo-img49" class="regalo_49"></div>
+            <div id="mapa-btn-pasos-regalo-img50" class="regalo_50"></div>
+            <div id="mapa-btn-pasos-regalo-img51" class="regalo_51"></div>
+            <div id="mapa-btn-pasos-regalo-img52" class="regalo_52"></div>
+            <div id="mapa-btn-pasos-regalo-img53" class="regalo_53"></div>
+            <div id="mapa-btn-pasos-regalo-img54" class="regalo_54"></div>
+            <div id="mapa-btn-pasos-regalo-img55" class="regalo_55"></div>
+            <div id="mapa-btn-pasos-regalo-img56" class="regalo_56"></div>
+            <div id="mapa-btn-pasos-regalo-img57" class="regalo_57"></div>
+            <div id="mapa-btn-pasos-regalo-img58" class="regalo_58"></div>
+            <div id="mapa-btn-pasos-regalo-img59" class="regalo_59"></div>
+            <div id="mapa-btn-pasos-regalo-img60" class="regalo_60"></div>
+            <div id="mapa-btn-pasos-regalo-img61" class="regalo_61"></div>
+            <div id="mapa-btn-pasos-regalo-img62" class="regalo_62"></div>
+            <div id="mapa-btn-pasos-regalo-img63" class="regalo_63"></div>
+            <div id="mapa-btn-pasos-regalo-img64" class="regalo_64"></div>
+            <div id="mapa-btn-pasos-regalo-img65" class="regalo_65"></div>
+            <div id="mapa-btn-pasos-regalo-img66" class="regalo_66"></div>
+            <div id="mapa-btn-pasos-regalo-img67" class="regalo_67"></div>
+            <div id="mapa-btn-pasos-regalo-img68" class="regalo_68"></div>
+            <div id="mapa-btn-pasos-regalo-img69" class="regalo_69"></div>
+            <div id="mapa-btn-pasos-regalo-img70" class="regalo_70"></div>
+            <div id="mapa-btn-pasos-regalo-img71" class="regalo_71"></div>
+            <div id="mapa-btn-pasos-regalo-img72" class="regalo_72"></div>
+            <div id="mapa-btn-pasos-regalo-img73" class="regalo_73"></div>
+            <div id="mapa-btn-pasos-regalo-img74" class="regalo_74"></div>
+            <div id="mapa-btn-pasos-regalo-img75" class="regalo_75"></div>
+            <div id="mapa-btn-pasos-regalo-img76" class="regalo_76"></div>
+            <div id="mapa-btn-pasos-regalo-img77" class="regalo_77"></div>
+            <div id="mapa-btn-pasos-regalo-img78" class="regalo_78"></div>
+            <div id="mapa-btn-pasos-regalo-img79" class="regalo_79"></div>
+            <div id="mapa-btn-pasos-regalo-img80" class="regalo_80"></div>
+            <div id="mapa-btn-pasos-regalo-img81" class="regalo_81"></div>
+            <div id="mapa-btn-pasos-regalo-img82" class="regalo_82"></div>
+            <div id="mapa-btn-pasos-regalo-img83" class="regalo_83"></div>
+            <div id="mapa-btn-pasos-regalo-img84" class="regalo_84"></div>
+            <div id="mapa-btn-pasos-regalo-img85" class="regalo_85"></div>
+            <div id="mapa-btn-pasos-regalo-img86" class="regalo_86"></div>
+            <div id="mapa-btn-pasos-regalo-img87" class="regalo_87"></div>
+            <div id="mapa-btn-pasos-regalo-img88" class="regalo_88"></div>
+            <div id="mapa-btn-pasos-regalo-img89" class="regalo_89"></div>
+            <div id="mapa-btn-pasos-regalo-img90" class="regalo_90"></div>
+            <div id="mapa-btn-pasos-regalo-img91" class="regalo_91"></div>
     </a>
 </div>
         <div class="animation" id="tamaulipas"></div>
