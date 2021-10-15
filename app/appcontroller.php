0b2730b53a2bba24e21f79df6f354823b1fffcbf
Carlos Jug <carlos@jug.mx>
2021-10-15 17:56:15 -0500

    ajustes seo

--- app/appcontroller.php
+++ app/appcontroller.php
@@ -8,6 +8,9 @@
 		$this->view->function = $this->action;
 		$this->view->params = $this->params;
 		$this->view->controller = $this->controller;
+		$this->meta_description("En la Expo Vivienda Virtual 2021 de JAVER encontrarás casas en venta en los mejores fraccionamientos del país. ¡Puedes comprar con tu crédito INFONAVIT!");
+		/*$this->meta_keywords("");*/
+		$this->meta_title("Mundo JAVER ¡Las mejores casas en venta! | JAVER");
 	}
 	
 } ?>
--- app/views/layouts/clean.php
+++ app/views/layouts/clean.php
@@ -41,7 +41,7 @@
 			<div class="top-planta container mb-0"><img class="mr-2" id="img-hijo" src="<?=Path?>/images/general/enredaderas.png"></div>
 				<div class="col-12 text-center pt-0 pb-0 contenido-logo-contador ">
 					<div class="col-8 col-lg-2 col-xl-2 offset-2 offset-lg-5 offset-xl-5 text-center py-0 mb-5" id="logo-clean">
-						<img class="mr-2" id="img-hijo" src="<?=Path?>/images/contador/contador_logo.svg"> 
+						<img class="mr-2" id="img-hijo" alt="MUNDO JAVER 2021" title="Conoce todas las casas en venta en Mundo JAVER" src="<?=Path?>/images/contador/contador_logo.svg"> 
 					</div>
 				</div>
 			<?
@@ -50,7 +50,7 @@
 			<div class="top-planta container mb-0"><img class="mr-2" id="img-hijo" src="<?=Path?>/images/general/enredaderas.png"></div>
 			<div class="col-12 text-center pt-0 pb-0 contenido-logo-contador justify-content-center d-flex">
 					<div class="col-4 col-lg-2 col-xl-2 text-center py-0 mb-0 img-preheat">
-						<img class="mr-2" id="img-hijo" src="<?=Path?>/images/contador/contador_logo.svg">
+						<img class="mr-2" id="img-hijo" alt="MUNDO JAVER 2021" title="Conoce todas las casas en venta en Mundo JAVER" src="<?=Path?>/images/contador/contador_logo.svg">
 					</div>
 				</div>
 			<?
