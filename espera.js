6aa5a5b10dc6b72397df149b9adb4851b6e0692c
Carlos Jug <carlos@jug.mx>
2021-10-28 17:43:56 -0500

    ajuste url absoluta para cuando se usa desde fuera

--- espera.js
+++ espera.js
@@ -1,21 +1,21 @@
 $('body').append('<div id="abandono" style="display:none;"></div>')
-$('#abandono').load(Path+'/index/abandono');
-/*
-$.ajax({
-    url: Path+'/index/abandono',
+$('#abandono').load('https://www.mundojaver.com.mx/index/abandono');
+
+/*$.ajax({
+    url: 'https://www.mundojaver.com.mx/index/abandono',
     dataType: 'html',
     success: function(html) {
         $('#abandono').html(html)
     }
-});
-  */
+});*/
+  
 $(document).mouseleave(function () {
     $('#abandono').show();
 });
 $(document).on('click','#btn-back-abandono',function(){
     $('#abandono').css({'display':'none'});
     $.ajax({
-        url: Path+'/index/abandono_close',
+        url: 'https://www.mundojaver.com.mx/index/abandono_close',
         dataType: 'html',
         success: function(html) {
             $('#abandono').remove();
