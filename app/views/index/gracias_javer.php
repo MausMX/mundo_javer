7fc22d31d80414582702cd6dc60b1cd4831d85ae
Carlos Jug <carlos@jug.mx>
2021-10-29 13:37:16 -0500

    ajuste texto y orden de gracias javer

--- app/views/index/gracias_javer.php
+++ app/views/index/gracias_javer.php
@@ -2,9 +2,9 @@
 	<div class="col-12 d-flex justify-content-center">
 		<div class="col-lg-8 col-xl-8 col-sm-12 col-md-10 col-xs-12 text-center contenido-contador">
 			<p class="poppins c-white bold font-72 title-preheat mb-4 participa">¡Gracias por participar!</p>
+			<p class="poppins c-white bold mb-0 mt-5 font-21">Felicidades por adquirir el hogar de tus sueños y ser parte de Mundo Javer.</p>
+			<p class="poppins c-white mb-0 font-21">Espera nuestra edición 2022, con más sorpresas y opciones para ti.</p>
 			<p class="poppins c-white font-21 mb-2 regalo-preheat-gracias" style="margin-top:-15px;"><img class="mr-2" id="img-hijo" src="<?=Path?>/images/general/img-gracias.png"></p>
-			<p class="poppins c-white bold mb-0 mt-5 font-18">Gracias por adquirir el hogar de tus sueños y ser parte de Mundo Javer 2021.</p>
-			<p class="poppins c-white mb-0 font-18">Espera nuestra edición 2022 con más sorpresas y opciones para ti.</p>
 		</div>
 	</div>
 </div>
